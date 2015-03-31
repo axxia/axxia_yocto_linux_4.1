de9730c77f87fbdb8082c7a0e7c55a061f2c5975
Charlie Paul <cpaul.windriver@gmail.com>
2015-03-31 16:26:20 -0700

    arch/arm: arm changes to support the axxia BSP
    These files were changed to support the LSI axxia 5500 board.
    
    Signed-off-by: Charlie Paul <cpaul.windriver@gmail.com>

--- init/main.c
+++ init/main.c
@@ -494,6 +494,17 @@ asmlinkage __visible void __init start_kernel(void)
 	char *command_line;
 	char *after_dashes;
 
+#if defined(CONFIG_ARCH_AXXIA) && defined(DEBUG_LL)
+	{
+		*(unsigned long *)(0xf0080000 + 0x24) = 13;
+		*(unsigned long *)(0xf0080000 + 0x28) = 1;
+		*(unsigned long *)(0xf0080000 + 0x2c) = 0x70;
+		*(unsigned long *)(0xf0080000 + 0x30) = 0x301;
+		*(unsigned long *)(0xf0080000 + 0x34) = 0;
+		*(unsigned long *)(0xf0080000 + 0x38) = 0x700;
+	}
+#endif	/* CONFIG_ARCH_AXXIA && DEBUG_LL */
+
 	/*
 	 * Need to run as early as possible, to initialize the
 	 * lockdep hash:
