5f0ec5e28d4ae562c56e781a5b8cf81d4d9bbbad
Vinod Koul <vinod.koul@intel.com>
2016-01-08 18:09:23 +0530

    ALSA: hdac: add link pm and ref counting
    The HDA links can be switched off when not is use, similarly
    command DMA can be stopped as well. This calls for a reference
    counting mechanism on the link by its users to manage the link
    power. The DMA can be turned off when all links are off
    
    For this we add two APIs
    	snd_hdac_ext_bus_link_get
    	snd_hdac_ext_bus_link_put
    
    They help users to turn up/down link and manage the DMA as well
    
    Change-Id: Iee67566c37887c97d190e06bcfdae11581aa7130
    Tracked-On: https://jira01.devtools.intel.com/browse/OAM-18711
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Reviewed-on: https://android.intel.com:443/467275
    Reviewed-on: https://git-gar-1.devtools.intel.com/gerrit/8251
    Reviewed-by: Babu, Ramesh <ramesh.babu@intel.com>
    Tested-by: Babu, Ramesh <ramesh.babu@intel.com>

--- include/sound/hdaudio_ext.h
+++ include/sound/hdaudio_ext.h
@@ -13,6 +13,8 @@
  * @mlcap: MultiLink capabilities pointer
  * @gtscap: gts capabilities pointer
  * @hlink_list: link list of HDA links
+ * @lock: lock for link mgmt
+ * @cmd_io: state of cmd_io
  */
 struct hdac_ext_bus {
 	struct hdac_bus bus;
@@ -25,6 +27,9 @@ struct hdac_ext_bus {
 	void __iomem *gtscap;
 
 	struct list_head hlink_list;
+
+	spinlock_t lock;
+	int cmd_io;
 };
 
 int snd_hdac_ext_bus_init(struct hdac_ext_bus *sbus, struct device *dev,
@@ -123,6 +128,9 @@ struct hdac_ext_link {
 	void __iomem *ml_addr; /* link output stream reg pointer */
 	u32 lcaps;   /* link capablities */
 	u16 lsdiid;  /* link sdi identifier */
+
+	int ref_count;
+
 	struct list_head list;
 };
 
@@ -133,6 +141,11 @@ void snd_hdac_ext_link_set_stream_id(struct hdac_ext_link *link,
 				 int stream);
 void snd_hdac_ext_link_clear_stream_id(struct hdac_ext_link *link,
 				 int stream);
+
+int snd_hdac_ext_bus_link_get(struct hdac_ext_bus *ebus,
+				struct hdac_ext_link *link);
+int snd_hdac_ext_bus_link_put(struct hdac_ext_bus *ebus,
+				struct hdac_ext_link *link);
 
 /* update register macro */
 #define snd_hdac_updatel(addr, reg, mask, val)		\
--- sound/hda/ext/hdac_ext_controller.c
+++ sound/hda/ext/hdac_ext_controller.c
@@ -180,6 +180,9 @@ int snd_hdac_ext_bus_get_ml_capabilities(struct hdac_ext_bus *ebus)
 		hlink->lcaps  = readl(hlink->ml_addr + AZX_REG_ML_LCAP);
 		hlink->lsdiid = readw(hlink->ml_addr + AZX_REG_ML_LSDIID);
 
+		/* since link in On, update the ref */
+		hlink->ref_count = 1;
+
 		list_add_tail(&hlink->list, &ebus->hlink_list);
 	}
 
@@ -300,3 +303,62 @@ int snd_hdac_ext_bus_link_power_down_all(struct hdac_ext_bus *ebus)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(snd_hdac_ext_bus_link_power_down_all);
+
+int snd_hdac_ext_bus_link_get(struct hdac_ext_bus *ebus,
+				struct hdac_ext_link *link)
+{
+	int ret = 0;
+
+	spin_lock(&ebus->lock);
+
+	/*
+	 * if we move from 0 to 1, count will be 1 so power up this link
+	 * as well, also check the dma status and trigger that
+	 */
+	if (++link->ref_count == 1) {
+		if (!ebus->cmd_io) {
+			snd_hdac_bus_init_cmd_io(&ebus->bus);
+			ebus->cmd_io = 1;
+		}
+
+		ret = snd_hdac_ext_bus_link_power_up(link);
+	}
+
+	spin_unlock(&ebus->lock);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(snd_hdac_ext_bus_link_get);
+
+int snd_hdac_ext_bus_link_put(struct hdac_ext_bus *ebus,
+				struct hdac_ext_link *link)
+{
+	int ret = 0, state = 0;
+	struct hdac_ext_link *hlink = NULL;
+
+	spin_lock(&ebus->lock);
+
+	/*
+	 * if we move from 1 to 0, count will be 0
+	 * so power down this link as well
+	 */
+	if (--link->ref_count == 0) {
+		ret = snd_hdac_ext_bus_link_power_down(link);
+
+		/*
+		 * now check if all links are off, if so turn off
+		 * cmd dma as well
+		 */
+		list_for_each_entry(hlink, &ebus->hlink_list, list) {
+			if (hlink->ref_count)
+				state++;
+		}
+		if (!state) {
+			snd_hdac_bus_stop_cmd_io(&ebus->bus);
+			ebus->cmd_io = 0;
+		}
+	}
+
+	spin_unlock(&ebus->lock);
+	return ret;
+}
+EXPORT_SYMBOL_GPL(snd_hdac_ext_bus_link_put);
