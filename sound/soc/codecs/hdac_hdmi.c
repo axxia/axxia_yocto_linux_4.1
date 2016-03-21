77c5b5d623b47c631848c6ca42891a960a8f9a05
Senthilnathan Veppur <senthilnathanx.veppur@intel.com>
2016-03-21 17:17:05 +0530

    ASoC: Codecs: Add NULL check
    Add NULL check before dereferencing inside the
    function called in the next line.
    
    Change-Id: I59ac4907a198132ec96231b5f92f51a9abcec902
    Signed-off-by: Senthilnathan Veppur <senthilnathanx.veppur@intel.com>

--- sound/soc/codecs/hdac_hdmi.c
+++ sound/soc/codecs/hdac_hdmi.c
@@ -537,6 +537,10 @@ static int hdmi_codec_probe(struct snd_soc_codec *codec)
 	 * exit, we call pm_runtime_suspend() so that will do for us
 	 */
 	hlink = snd_hdac_ext_bus_get_link(edev->ebus, dev_name(&edev->hdac.dev));
+	if (!hlink) {
+		dev_err(&edev->hdac.dev, "Cannot find link bus for codec %s\n",dev_name(&edev->hdac.dev));
+		return -EINVAL;
+	}
 	snd_hdac_ext_bus_link_get(edev->ebus, hlink);
 
 	create_fill_widget_route_map(dapm, &hdmi->dai_map[0]);
@@ -607,6 +611,10 @@ static int hdac_hdmi_dev_probe(struct hdac_ext_device *edev)
 
 	/* hold the ref while we probe */
 	hlink = snd_hdac_ext_bus_get_link(edev->ebus, dev_name(&edev->hdac.dev));
+	if (!hlink) {
+		dev_err(&codec->dev, "Cannot find link bus for codec %s\n",dev_name(&edev->hdac.dev));
+		return -EINVAL;
+	}
 	snd_hdac_ext_bus_link_get(edev->ebus, hlink);
 
 	hdmi_priv = devm_kzalloc(&codec->dev, sizeof(*hdmi_priv), GFP_KERNEL);
@@ -678,6 +686,10 @@ static int hdac_hdmi_runtime_suspend(struct device *dev)
 		dev_err(bus->dev, "Cannot turn on display power on i915\n");
 
 	hlink = snd_hdac_ext_bus_get_link(ebus, dev_name(dev));
+	if (!hlink) {
+		dev_err(bus->dev, "Cannot find link bus for codec %s\n",dev_name(dev));
+		return -EINVAL;
+	}
 	snd_hdac_ext_bus_link_put(ebus, hlink);
 
 	return err;
@@ -699,6 +711,10 @@ static int hdac_hdmi_runtime_resume(struct device *dev)
 		return 0;
 
 	hlink = snd_hdac_ext_bus_get_link(ebus, dev_name(dev));
+	if (!hlink) {
+		dev_err(bus->dev, "Cannot find link bus for codec %s\n",dev_name(dev));
+		return -EINVAL;
+	}
 	snd_hdac_ext_bus_link_get(ebus, hlink);
 
 	err = snd_hdac_display_power(bus, true);
