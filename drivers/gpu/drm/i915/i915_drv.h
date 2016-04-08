625e7620c86cf1b8c761bf2a05fdbca364e7a5a2
Dave Gordon <david.s.gordon@intel.com>
2016-04-08 13:52:57 -0700

    drm/i915/error: capture execlist state on error
    At present, execlist status/ctx_id and CSBs, not the submission queue
    
    For: VIZ-2021
    Signed-off-by: Dave Gordon <david.s.gordon@intel.com>
    Signed-off-by: Jeff McGee <jeff.mcgee@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -521,6 +521,15 @@ struct drm_i915_error_state {
 		u32 rc_psmi; /* sleep state */
 		u32 semaphore_mboxes[I915_NUM_ENGINES - 1];
 
+		/* Execlists */
+		u32 execlist_status;
+		u32 execlist_ctx_id;
+		u32 execlist_csb_raw_pointer;
+		u32 execlist_csb_write_pointer;
+		u32 execlist_csb_read_pointer;
+		u32 execlist_csb[6];
+		u32 execlist_ctx[6];
+
 		struct drm_i915_error_object {
 			int page_count;
 			u64 gtt_offset;
