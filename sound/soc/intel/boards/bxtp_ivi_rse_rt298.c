7aabfcd9470b4d38822d323bd3db691a288523e9
Mousumi Jana <mousumix.jana@intel.com>
2016-04-27 23:09:29 +0530

    ASoC: Intel: Boards: Add probe DAI Link in IVI RSE Machine driver.
    Introduce the DAI Link for probe in RSE machine driver.
    
    Change-Id: I07e395a1f1b4d4c4b9d623d5a671d0d567d2302c
    Signed-off-by: Mousumi Jana <mousumix.jana@intel.com>

--- sound/soc/intel/boards/bxtp_ivi_rse_rt298.c
+++ sound/soc/intel/boards/bxtp_ivi_rse_rt298.c
@@ -32,8 +32,6 @@ static const struct snd_soc_dapm_widget broxton_widgets[] = {
 	SND_SOC_DAPM_SPK("Speaker2", NULL),
 	SND_SOC_DAPM_SPK("Speaker3", NULL),
 	SND_SOC_DAPM_MIC("DMIC2", NULL),
-	SND_SOC_DAPM_SIGGEN("probe source"),
-	SND_SOC_DAPM_SINK("probe sink"),
 };
 
 static const struct snd_soc_dapm_route broxton_rt298_map[] = {
@@ -52,8 +50,6 @@ static const struct snd_soc_dapm_route broxton_rt298_map[] = {
 	{ "ssp1 Tx", NULL, "codec3_out"},
 	{ "hdmi_ssp0_in", NULL, "ssp0 Rx" },
 	{ "ssp0 Rx", NULL, "Dummy Capture1" },
-	{ "extractor cpr 8", NULL, "probe source"},
-	{ "probe sink", NULL, "injector cpr 9"},
 };
 
 static int bxtp_ssp0_gpio_init(struct snd_soc_pcm_runtime *rtd)
@@ -297,28 +293,27 @@ static struct snd_soc_dai_link broxton_rt298_dais[] = {
 		.capture_only = true,
 		.ignore_suspend = 1,
 	},
-        {
-                .name = "Bxt Probe playback",
-                .stream_name = "Probe Playback",
-                .cpu_dai_name = "Probe0 Pin",
-                .codec_name = "snd-soc-dummy",
+	{
+		.name = "Bxt Compress Probe playback",
+		.stream_name = "Probe Playback",
+		.cpu_dai_name = "Compress Probe0 Pin",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.platform_name = "0000:00:0e.0",
+		.init = NULL,
+		.nonatomic = 1,
+	},
+	{
+		.name = "Bxt Compress Probe capture",
+		.stream_name = "Probe Capture",
+		.cpu_dai_name = "Compress Probe1 Pin",
+		.codec_name = "snd-soc-dummy",
                 .codec_dai_name = "snd-soc-dummy-dai",
                 .platform_name = "0000:00:0e.0",
                 .init = NULL,
-                .ignore_suspend = 1,
                 .nonatomic = 1,
         },
-        {
-                .name = "Bxt Probe capture",
-                .stream_name = "Probe Capture",
-                .cpu_dai_name = "Probe1 Pin",
-                .codec_name = "snd-soc-dummy",
-                .codec_dai_name = "snd-soc-dummy-dai",
-                .platform_name = "0000:00:0e.0",
-                .init = NULL,
-                .ignore_suspend = 1,
-                .nonatomic = 1,
-        },
+
 
 
 
