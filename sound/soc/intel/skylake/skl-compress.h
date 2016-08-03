1751366fd5daf57deb3325aa16e2b589a8cc9f97
Jayachandran B <jayachandran.b@intel.com>
2016-08-03 16:44:22 +0530

    ASoC: Intel: Skylake: Probe - Fix function inlining issue
    get_hdac_ext_compr_stream was defined in a c file,
    but was declared as inline in a header file.
    This caused compilation issue when CONFIG_OPTIMIZE_INLINING
    option was turned off.
    
    Moved the function definition to the header file.
    
    Change-Id: I0bd1900e6b30f8c86a335714d11a74faef1cd8ef
    Signed-off-by: Jayachandran B <jayachandran.b@intel.com>
    Reviewed-on: https://git-gar-1.devtools.intel.com/gerrit/10744
    Reviewed-by: Jayanti, Satya Charitardha <satya.charitardha.jayanti@intel.com>
    Tested-by: Jayanti, Satya Charitardha <satya.charitardha.jayanti@intel.com>

--- sound/soc/intel/skylake/skl-compress.h
+++ sound/soc/intel/skylake/skl-compress.h
@@ -19,8 +19,19 @@
  *
  */
 
-inline
-struct hdac_ext_stream *get_hdac_ext_compr_stream(struct snd_compr_stream *stream);
+#ifndef __SKL_COMPRESS_H__
+#define __SKL_COMPRESS_H__
+
+#include <sound/soc.h>
+#include <sound/memalloc.h>
+#include "skl.h"
+
+static inline
+struct hdac_ext_stream *get_hdac_ext_compr_stream(struct snd_compr_stream *stream)
+{
+	return stream->runtime->private_data;
+}
+
 struct hdac_ext_bus *get_bus_compr_ctx(struct snd_compr_stream *substream);
 void skl_set_compr_runtime_buffer(struct snd_compr_stream *substream,
 				struct snd_dma_buffer *bufp, size_t size);
@@ -32,4 +43,4 @@ int skl_substream_alloc_compr_pages(struct hdac_ext_bus *ebus,
 int skl_compr_free_pages(struct snd_compr_stream *substream);
 int skl_substream_free_compr_pages(struct hdac_bus *bus,
 				struct snd_compr_stream *substream);
-
+#endif
