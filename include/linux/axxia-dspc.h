3018ee32767b500e06ea4ed514c2bdb6cfdc3c59
John Jacques <john.jacques@intel.com>
2016-10-04 08:29:55 -0500

    axxia: Add DSP Cluster Control to the Axxia 6700 Platform
    There are OEM functions in the secure monitor to control DSP
    cluster power and coherency.  This commit adds the ability
    to control DSP cluster power and coherency from Linux.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>

---
+++ include/linux/axxia-dspc.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2016 Intel <john.jacques@intel.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307	 USA
+ */
+
+#ifndef __DRIVERS_MISC_AXXIA_DSPC_H
+#define __DRIVERS_MISC_AXXIA_DSPC_H
+
+unsigned long axxia_dspc_get_state(void);
+void axxia_dspc_set_state(unsigned long);
+
+#endif /* __DRIVERS_MISC_AXXIA_DSPC_H */
