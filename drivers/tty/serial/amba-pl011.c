632a0003a6b79d4d2beef88a205545be4bacf047
John Jacques <john.jacques@intel.com>
2016-11-16 18:56:38 +0200

    drivers/tty/serial: Remove a Bringup Change
    During Axxia bringup, setting the baud rate was disabled because
    the frequency reported for the peripheral clock was wrong.
    
    This commits reverts that change.
    
    Signed-off-by: John Jacques <john.jacques@intel.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- drivers/tty/serial/amba-pl011.c
+++ drivers/tty/serial/amba-pl011.c
@@ -1820,10 +1820,8 @@ pl011_set_termios(struct uart_port *port, struct ktermios *termios,
 			quot -= 2;
 	}
 	/* Set baud rate */
-#if 0
 	writew(quot & 0x3f, port->membase + UART011_FBRD);
 	writew(quot >> 6, port->membase + UART011_IBRD);
-#endif
 
 	/*
 	 * ----------v----------v----------v----------v-----
