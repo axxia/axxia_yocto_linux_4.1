2ef221bebe028cbdf76482419f94614eaa7f6eef
Chris Wilson <chris@chris-wilson.co.uk>
2016-12-13 13:22:34 -0800

    drm/i915/opregion: Rename init/fini functions to register/unregister
    Current intel_opregion_init is called during the driver registration
    phase and intel_opregion_fini from the unregistration phase. Rename the
    functions so that this is clear from their names. The phases tell us
    what we expect the existing hw state to be, e.g. whether interrupts are
    still enabled etc.
    
    It should be noted that the opregion init/fini routines are asymmetric
    and this is carried across into their new names. Indeed, their new names
    make it even clearer that perhaps all is not well in the opregion
    suspend/resume sequence (as well in the module unload).
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Jani Nikula <jani.nikula@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1464012490-30961-2-git-send-email-chris@chris-wilson.co.uk
    Reviewed-by: Jani Nikula <jani.nikula@linux.intel.com>
    (cherry picked from commit 03d92e4779b82e03f3fbf6631156eb0727fc75d4)
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/intel_opregion.c
+++ drivers/gpu/drm/i915/intel_opregion.c
@@ -782,7 +782,7 @@ static void intel_setup_cadls(struct drm_i915_private *dev_priv)
 	} while (++i < 8 && disp_id != 0);
 }
 
-void intel_opregion_init(struct drm_i915_private *dev_priv)
+void intel_opregion_register(struct drm_i915_private *dev_priv)
 {
 	struct intel_opregion *opregion = &dev_priv->opregion;
 
@@ -809,7 +809,7 @@ void intel_opregion_init(struct drm_i915_private *dev_priv)
 	}
 }
 
-void intel_opregion_fini(struct drm_i915_private *dev_priv)
+void intel_opregion_unregister(struct drm_i915_private *dev_priv)
 {
 	struct intel_opregion *opregion = &dev_priv->opregion;
 
