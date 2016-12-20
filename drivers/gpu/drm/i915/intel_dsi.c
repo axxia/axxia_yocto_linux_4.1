677552e1c96c6095cb4ae44889ab9b45e92bcf85
Uma Shankar <uma.shankar@intel.com>
2016-12-20 10:24:26 -0800

    drm/i915: Check for platform specific GPIO config
    Panel GPIO control should be done based on platform. Add a check
    to restrict BYT specific GPIO confirguration for only BYT. Avoid
    the same for BXT.
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Bob Paauwe <bob.j.paauwe@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/intel_dsi.c
+++ drivers/gpu/drm/i915/intel_dsi.c
@@ -1584,7 +1584,8 @@ void intel_dsi_init(struct drm_device *dev)
 	 * In case of BYT with CRC PMIC, we need to use GPIO for
 	 * Panel control.
 	 */
-	if (dev_priv->vbt.dsi.config->pwm_blc == PPS_BLC_PMIC) {
+	if (!IS_BROXTON(dev) &&
+		(dev_priv->vbt.dsi.config->pwm_blc == PPS_BLC_PMIC)) {
 		intel_dsi->gpio_panel =
 			gpiod_get(dev->dev, "panel", GPIOD_OUT_HIGH);
 
